73b5e28ac34c05e59bfcbfd73927b37317c51fa9
Santanu Barman <barmansantanu11@gmail.com>
2023-01-24 18:36:00 +0530

    added lazy loading to the images

--- src/core/Common components/CardImageHelper.js
+++ src/core/Common components/CardImageHelper.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { API } from "../../Backend";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const CardImageHelper = ( { product } ) => {
     const imageURL = product ?  `${API}/product/photo/${product._id}` : "https://images.pexels.com/photos/4659797/pexels-photo-4659797.jpeg?auto=compress&cs=tinysrgb&w=1600";
   
     return (
     <div className='card-image'>
-            <img src={imageURL} alt={'book_img'} />
+            <LazyLoadImage loading='lazy' src={imageURL} alt={'book_img'} />
     </div>
   )
 }
